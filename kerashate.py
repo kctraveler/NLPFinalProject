63506bc44ea3ce2e827e7feb289dc02613882f6e
metaksu <met-aksu@hotmail.com>
2021-04-21 19:35:23 -0500

    First Commit

---
+++ kerashate.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from zipfile import ZipFile
+import re
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+import string
+from sklearn.linear_model import SGDClassifier
+import logging
+from textblob import TextBlob
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import warnings;warnings.filterwarnings('ignore')
+import preprocess
+#from kerasLSTM import KerasLSTMClassifier
+
+hate_speech_corpus = pd.read_csv("hate_speech.csv")
+#Shape
+print("Shape: ", hate_speech_corpus.shape)
+#Class distribution
+print("Class Values:\n", hate_speech_corpus['class'].value_counts())
+
+hate_speech_corpus_final = hate_speech_corpus[['class', 'tweet']]
+
+print(hate_speech_corpus_final[0:5])
+
+X = hate_speech_corpus_final[['tweet']]
+y = hate_speech_corpus_final[['class']]
+
+sns.barplot(['Non Toxic', 'Toxic', 'Hate'], hate_speech_corpus_final['class'].map({0:"Non Toxic", 1: "Toxic", 2: "Hate"}).value_counts(), palette="icefire")
+plt.title('Count of Toxic and Hate Comments of Dataset')
+plt.show()
+
+#Testing cleaner
+for idx in hate_speech_corpus_final.tail(15).index:
+  print(preprocess.cleaner(hate_speech_corpus_final.iloc[idx]['tweet']),'\n'  , hate_speech_corpus_final.iloc[idx]['tweet'], idx)
+  print("************")
+
+# import spacy
+# from keras.preprocessing.text import Tokenizer
+# #!python -m spacy download en_core_web_lg
+# nlp = spacy.load('/usr/local/lib/python3.6/dist-packages/en_core_web_lg/en_core_web_lg-2.1.0')
+# #Embedding
+# tokenizer = Tokenizer(num_words=30000)
+# tokenizer = Tokenizer(num_words=30000)
+# tokenizer.fit_on_texts(hate_speech_corpus_final['tweet'])
+# embeddings_index = np.zeros((30000 + 1, 300))
+# for word, idx in tokenizer.word_index.items():
+#     try:
+#           embedding = nlp.vocab[word].vector
+#           embeddings_index[idx] = embedding
+#     except:
+#       pass
+
+# lstmMODEL = KerasLSTMClassifier()
---
+++ preprocess.py
@@ -0,0 +1,60 @@
+import re
+import string
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+
+#NLTK Stop words
+stop_words_ = set(stopwords.words('english'))
+#Custom Stop Words
+my_sw = ['rt', 'ht', 'fb', 'amp', 'gt']
+#WordNetLemma
+wn = WordNetLemmatizer()
+
+def slang_txt(token):
+  if token == 'u':
+    token = 'you'
+  return  token not in stop_words_ and token not in list(string.punctuation) and token not in my_sw
+
+def cleaner(word):
+  #Remove links
+  word = re.sub(r'((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*', 
+                '', word, flags=re.MULTILINE)
+  #Decontracted words
+  word = decontracted(word)
+  #Remove users mentions
+  word = re.sub(r'(@[^\s]*)', "", word)
+  word = re.sub('[\W]', ' ', word)
+  #Lemmatized
+  list_word_clean = []
+  for w1 in word.split(" "):
+    if  slang_txt(w1.lower()):
+      word_lemma =  wn.lemmatize(w1,  pos="v")
+      list_word_clean.append(word_lemma)
+  #Cleaning, lowering and remove whitespaces
+  word = " ".join(list_word_clean)
+  word = re.sub('[^a-zA-Z]', ' ', word)
+  return word.lower().strip()
+
+def decontracted(phrase):
+    phrase = re.sub(r"https?://[A-Za-z0-9./]+", "url", phrase)
+    phrase = re.sub(r"[^A-Za-z0-9^,!./\'+-=]", "", phrase)
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"what's", "what is", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+    phrase = re.sub(r"j k", "jk", phrase)
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
