6dc2bbb9ea946accb183fa776388867b2eb89d49
metaksu <met-aksu@hotmail.com>
2021-05-04 18:17:35 -0500

    lstm complete

--- kerashate.py
+++ kerashate.py
@@ -24,9 +24,10 @@ import logging
 from textblob import TextBlob
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from keras.utils import np_utils
 import warnings;warnings.filterwarnings('ignore')
 import preprocess
-#from kerasLSTM import KerasLSTMClassifier
+from kerasLSTM import KerasLSTMClassifier
 
 hate_speech_corpus = pd.read_csv("hate_speech.csv")
 #Shape
@@ -40,24 +41,29 @@ print(hate_speech_corpus_final[0:5])
 
 X = hate_speech_corpus_final[['tweet']]
 y = hate_speech_corpus_final[['class']]
-
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+Y = np_utils.to_categorical(y)
+X = X.values
+X = [x[0] for x in X]
 sns.barplot(['Non Toxic', 'Toxic', 'Hate'], hate_speech_corpus_final['class'].map({0:"Non Toxic", 1: "Toxic", 2: "Hate"}).value_counts(), palette="icefire")
 plt.title('Count of Toxic and Hate Comments of Dataset')
 plt.show()
 
 #Testing cleaner
-for idx in hate_speech_corpus_final.tail(15).index:
-  print(preprocess.cleaner(hate_speech_corpus_final.iloc[idx]['tweet']),'\n'  , hate_speech_corpus_final.iloc[idx]['tweet'], idx)
-  print("************")
+# for idx in hate_speech_corpus_final.tail(15).index:
+#   print(preprocess.cleaner(hate_speech_corpus_final.iloc[idx]['tweet']),'\n'  , hate_speech_corpus_final.iloc[idx]['tweet'], idx)
+#   print("************")
 
 import spacy
 from keras.preprocessing.text import Tokenizer
 #!python -m spacy download en_core_web_lg
 nlp = spacy.load("en_core_web_lg")
+
+
 #Embedding
 tokenizer = Tokenizer(num_words=30000)
-tokenizer = Tokenizer(num_words=30000)
-tokenizer.fit_on_texts(hate_speech_corpus_final['tweet'])
+tokenizer.fit_on_texts(X)
 embeddings_index = np.zeros((30000 + 1, 300))
 for word, idx in tokenizer.word_index.items():
     try:
@@ -65,5 +71,9 @@ for word, idx in tokenizer.word_index.items():
           embeddings_index[idx] = embedding
     except:
       pass
-print(embeddings_index[1])
-#lstmMODEL = KerasLSTMClassifier()
+
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 444, stratify=y)
+lstmMODEL = KerasLSTMClassifier(emb_idx= embeddings_index)
+print(lstmMODEL.model.summary())
+lstmMODEL.fit(x_train, y_train)
+# print(lstmMODEL.score(x_test, y_test))
