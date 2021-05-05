5570ee8065bea3f2790986275720c2649697667c
Maria Gocal <mgocalkappos@luc.edu>
2021-05-04 22:28:28 -0500

    update

--- kerasRNN.py
+++ kerasRNN.py
@@ -1,10 +1,9 @@
 import numpy as np
+from keras.engine.input_layer import InputLayer
 from keras.preprocessing.text import Tokenizer
 from preprocess import cleaner
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Model
-from keras.layers import Dense, Input, LSTM, Bidirectional, Activation, Conv1D, GRU, TimeDistributed
-from keras.layers import Dropout, Embedding, GlobalMaxPooling1D, MaxPooling1D, Add, Flatten, SpatialDropout1D
+from keras.layers import Embedding
 from keras.utils.np_utils import to_categorical
 from keras.layers import Dense, SimpleRNN, Activation
 from keras.models import Sequential
@@ -30,13 +29,16 @@ class KerasRNNClassifier:
 
     def _rnn(self):
         model = Sequential()
-        model.add(SimpleRNN(50, input_shape=(49, 1), return_sequences=True))
+        model.add(InputLayer((self.input_length,)))
+        model.add(SimpleRNN(
+            units=50, recurrent_dropout=0, activation="tanh", unroll=False,
+            use_bias=True))
         model.add(
            Embedding(
                input_dim=self.max_words, output_dim=self.emb_dim,
                input_length=self.input_length, mask_zero=False,
                trainable=False))
-        model.add(Dense(46))
+        model.add(Dense(3))
         model.add(Activation('softmax'))
 
         adam = optimizers.Adam(lr=0.001)
@@ -68,50 +70,3 @@ class KerasRNNClassifier:
         self.tokenizer.word_index[self.tokenizer.oov_token] = self.max_words + 1
         seqs = self._get_sequences(self._preprocess(X))
         self.model.fit([seqs], y, batch_size=self.bs, epochs=self.epochs, validation_split=0.3)
-
-# import numpy as np
-# import pandas as pd
-# from sklearn.metrics import accuracy_score
-# from keras.datasets import reuters
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.utils.np_utils import to_categorical
-# from keras.models import Sequential
-# from keras.layers import Dense, SimpleRNN, Activation
-# from keras import optimizers
-# from keras.wrappers.scikit_learn import KerasClassifier
-# from sklearn.model_selection import train_test_split
-#
-# # pad the sequences with zeros
-# # padding parameter is set to 'post' => 0's are appended to end of sequences
-# #
-# # X_train = pad_sequences(X_train, padding='post')
-# # X_test = pad_sequences(X_test, padding='post')
-#
-# X_train = np.array(X_train).reshape((X_train.shape[0], X_train.shape[1], 1))
-# X_test = np.array(X_test).reshape((X_test.shape[0], X_test.shape[1], 1))
-#
-# y_data = np.concatenate((y_train, y_test))
-# y_data = to_categorical(y_data)
-# y_train = y_data[:1395]
-# y_test = y_data[1395:]
-#
-#
-# def rnn():
-#     model = Sequential()
-#     model.add(SimpleRNN(50, input_shape=(49, 1), return_sequences=False))
-#     model.add(Dense(46))
-#     model.add(Activation('softmax'))
-#
-#     adam = optimizers.Adam(lr=0.001)
-#     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
-#
-#     return model
-#
-#
-# model = KerasClassifier(build_fn=rnn, epochs=200, batch_size=50, verbose=1)
-# model.fit(X_train, y_train)
-#
-# y_pred = model.predict(X_test)
-# y_test_ = np.argmax(y_test, axis=1)
-#
-# print(accuracy_score(y_pred, y_test_))
